7ec664c9c52b15eae67bca5afabd47de758361b0
B.D.Kim <bbd@mnutube.com>
2022-01-24 02:17:54 +0900

    fail

--- src/App.js
+++ src/App.js
@@ -5,16 +5,54 @@ class App extends Component {
     super(props)
 
     this.localVideoRef = React.createRef();
+    this.videoElem = React.createRef();
   }
 
   render() {
-    const constraints = { video: true };
+    // const constraints = { video: true };
 
-    navigator.getUserMedia( constraints, success, failure );
+    // const success = (stream) => {
+    //   this.localVideoRef.current.srcObject = stream;
+    // };
 
+    // const failure = (err) => {
+    //   console.log('getUserMedia Error: ', err);
+    // }
+
+    // navigator.mediaDevices.getUserMedia( constraints, success, failure );
+
+    const displayMediaOptions = {
+      video: {
+        cursor: "always"
+      },
+      audio: false
+    }
+
+    function dumpOptionsInfo() {
+      const videoTrack = this.videoEle.current.srcObject.getVideoTracks()[0];
+    }
+
+    async function startCapture() {
+      try {
+        this.videoElem.current.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+        dumpOptionsInfo();
+      } catch(err) {
+        console.error('startCapture Error', err);
+      }
+    }
+
+    function stopCapture(evt) {
+      let tracks = this.videoElem.current.srcObject.getTracks();
+
+      tracks.forEach(track => track.stop());
+      this.videoEle.srcObject = null;
+    }
+
+    
     return (
       <div>
-        <video ref={this.localVideoRef} autoPlay></video>
+        <button onClick={() => startCapture()}>Start Capture</button>
+        <video ref={this.videoElem} style={{width: '100%'}} autoPlay></video>
       </div>
     );
   };
